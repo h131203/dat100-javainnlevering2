82fb6f6bc3e8ba9ff6748ce4e0b0f79fc17cd45f
h_gri <h_gri@HGLaptop5>
2020-09-26 10:53:59 +0200

    alle oppgaver (eksl friviligge) utført

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -5,60 +5,173 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int i = 0; i < tabell.length; i++) {
+			System.out.println(tabell[i]);
+		}
+		// throw new UnsupportedOperationException("skrivUt ikke implementert");
 
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String utskrift = "";
+
+		for (int i = 0; i < tabell.length; i++) {
+			utskrift += tabell[i];
+			if (i<tabell.length-1) {
+				utskrift += ",";
+			}
+		}
+		utskrift = "[" + utskrift + "]";
+		return utskrift;
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+		int sumForLoop = 0; // for å vise hvilken sum som returneres fra for-løkken
+
+		// med for-løkke
+		for (int i = 0; i < tabell.length; i++) {
+			sumForLoop += tabell[i];
+		}
+
+		// med while-løkke
+
+		int i = 0;
+		int sumWhileLoop = 0; // for å vise hvilken sum som returneres fra while-løkken.
+
+		while (i < tabell.length) {
+			sumWhileLoop += tabell[i];
+			i++;
+		}
+
+		// med utvidet for-løkke
+		int sumUtvidetForLoop = 0; // for å vise hvilken sum som returneres fra utvidet for-løkke
+		for (int num : tabell) {
+			sumUtvidetForLoop = sumUtvidetForLoop + num;
+		}
+		/*
+		 * System.out.print("for loop:  " + sumForLoop);
+		 * System.out.print("utvidet for loop: " + sumUtvidetForLoop);
+		 * System.out.print("while gir lengde..:" + sumWhileLoop);
+		 */
+
+		return sumForLoop; //eller sumWhileLoop, eller sumUtvidetForLoop
+
 	}
 
 	// d)
+	/*
+	 * Implementer en metode: public static boolean finnesTall (int[] tabell, int
+	 * tall) som gitt en tabell og et tall returnerer true om tabellen tabell
+	 * innholder tallet tall og false ellers.
+	 * 
+	 * e)
+	 */
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+		int leterEtter = tall;
+		boolean funnet = false;
+		int i = 0;
+
+		while (!funnet && i < tabell.length) {
+			if (tabell[i] == leterEtter) {
+				funnet = true;
+			}
+			i++;
+		}
+
+		// throw new UnsupportedOperationException("finnesTall ikke implementert");
+		return funnet;
 	}
 
 	// e)
+	/*
+	 * Implementer en metode public static int posisjonTall (int[] tabell, int tall)
+	 * som returnerer det første index (posisjon) i tabell som inneholder tallet
+	 * tall og -1 ellers.
+	 */
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
+		int leterEtter = tall;
+		boolean funnet = false;
+		int i = 0;
+		int posisjon = 0;
+
+		while (!funnet && i < tabell.length) {
+
+			if (tabell[i] == leterEtter) {
+				funnet = true;
+				posisjon = i;
+				return posisjon;
+			}
+			i++;
+		}
+
+		return -1;
+		// throw new UnsupportedOperationException("posisjonTall ikke implementert");
 
 	}
 
 	// f)
+	/*
+	 * Implementer en metode public static int[] reverser(int[] tabell) som gitt en
+	 * tabell av heltall returnerer en ny tabell som har de samme elementene som
+	 * tabell men i modsatt rekkefølge.
+	 */
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
 		
+		int[] reversertTabell = new int[tabell.length];
+	
+		for (int i = 0; i < tabell.length; i++) {
+			reversertTabell[i] = tabell[tabell.length - i - 1];
+		}
+		
+		return reversertTabell;
+		// throw new UnsupportedOperationException("reverser ikke implementert");
+
 	}
 
-	// g)
+	// g) 
+	/*
+	 * Implementer en metode:
+	som kan avgjere om ein array (tabell) av heiltal er sortert stigande (dvs. at alle tal skal vere større enn det forrige)
+	 */
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		boolean sortert = true;
+		int i = 1;
+		
+		while (sortert && i < tabell.length) {
+			if (tabell[i-1] <= tabell[i]) {
+				i++;
+			} else {
+				sortert = false;
+			}
+		}
+		return sortert;
+		//throw new UnsupportedOperationException("erSortert ikke implementert");
 	}
 
 	// h)
+	/*
+	 * Implementer en metode:
+		som gitt to tabeller tabell1 og tabell2 som paramtere returnerer en ny tabell som er sammensetning av de to tabeller.
+	 */
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
 
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+		int nyLengde = tabell1.length + tabell2.length; //finner lengde totalt
+		int[] nyTabell = new int[nyLengde]; //oppretter ny tab med lengden fra de 2 det gjelder
+		for (int i = 0; i<tabell1.length; i++) {
+			nyTabell[i] = tabell1[i]; // "flytter" tabell1 over til nyTabell
+		}
+		for (int j = 0; j<tabell2.length; j++) {
+			nyTabell[tabell1.length+j] = tabell2[j]; //legger inn verdier fra tabell2 på første index ETTER LENGDEN på tabell 1.
+		}
+		return nyTabell;
+		
+		//throw new UnsupportedOperationException("settSammen ikke implementert");
 	}
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -3,33 +3,94 @@ package no.hvl.dat100.lab6.matriser;
 public class Matriser {
 
 	// a)
+	/*
+	 * Implementer en metode:
+public static void skrivUtv1(int[][] matrise)
+som kan skrive ut en matrise. Prøv å bruk to to (nøstede) utvidede for-løkker (se avsnitt 5.6 i boken for beskrivelse av utvidet for-løkke).
+	 */
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int[] i: matrise) {
+			for (int j: i) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}		
+		//throw new UnsupportedOperationException("skrivUt ikke implementert");
 	}
 
 	// b)
-	public static String tilStreng(int[][] matrise) {
+	/*
+	 * Implementer en metode
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+public static String tilStreng(int[][] matrise)
+som returnerer en streng-representation av en matrise. Om matrisen er følgende:
+
+{ {1,2,3}, {4,5,6}, {7,8,9} }
+da skal strengen som returneres ha følgende innhold:
+
+1 2 3 \n4 5 6 \n7 8 9 \n
+	 */
+	public static String tilStreng(int[][] matrise) {
+		
+		String matriseString = "";
+		
+		for (int[] i: matrise) {
+			for (int j: i) {
+				matriseString += j + " ";
+			}
+			matriseString += "\n";
+		}	
+		return matriseString;
+		
+		//throw new UnsupportedOperationException("tilStreng ikke implementert");
 		
 	}
 
 	// c)
+	/*
+	 * Implementer en metode
+public static int[][] skaler(int tall, int[][] matrise)
+som returnerer en ny matrise der alle tall i matrisen er multiplisert med parameteren tall. 
+Metoden må først opprette en matrise like stor som parameteren og så multiplisere alle elementer med tall.
+	 */
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
+		int[][] nyMatrise = new int[matrise.length][matrise.length];
+		
+		for(int i=0; i<matrise.length; i++) {
+			for(int j=0; j<matrise.length; j++) {
+					nyMatrise[i][j] += matrise[i][j] * tall;
+			}
+		}
+		
+		return nyMatrise;
+		//throw new UnsupportedOperationException("skaler ikke implementert");
 	
 	}
 
 	// d)
+	/*
+	 * Implementer en metode
+public static boolean erLik(int[][] mat1, int[][] mat2)
+som avgjør om to matriser gitt ved parametrene a og b er like.
+	 */
+	
 	public static boolean erLik(int[][] a, int[][] b) {
+		
+		boolean erLik = false;
+		
+		for (int i = 0; i<a.length; i++) {
+			for (int j = 0; j<b.length; j++) {
+				if (a[i][j] == b[i][j]) {
+					erLik = true;
+				}
+			}
+		}
+		
+		return erLik;
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		//throw new UnsupportedOperationException("erLik ikke implementert");
 	}
 	
 	// e)
